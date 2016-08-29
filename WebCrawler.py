d3af820d22532edad4b885795c95155abc47aea8
mandeman <abhijitmande1024@gmail.com>
2016-08-29 22:35:20 +0530

    Add files via upload

--- WebCrawler.py
+++ WebCrawler.py
@@ -1,34 +1,44 @@
 import urllib.request
 from bs4 import BeautifulSoup
 import requests
-import datetime
+import datetime                 #only to generate logname
 
-
+#Downloader logic. Accepts Image URL and storage location
 def downloader(addr, loc):
     name = addr[15:]
-    #print(addr)
-    ##print(name)
     urllib.request.urlretrieve('http:' + addr, loc + '\\' + name)
     return 1
+    #Why return 1?
 
-
+#Crawl logic
 def crawler(urlname, res_val):
-    source_code = requests.get(urlname)
-    plain_text = source_code.text
+    #Get source code from the webpage
+    source_code = requests.get(urlname).text
+
+    #Turn the source code into beautiful soup object
+    #Really beautiful tbh
+    soup = BeautifulSoup(source_code, "html.parser")
+
+    loc = input("Enter storage location : ")
 
     logname = str(datetime.datetime.now()).replace('.', "-")
     logname = logname.replace(':', '-')
-    #logname.replace(':', '-')
-    print (logname)
 
+    #How many images have been downloaded?
+    #Why 1? Because this has to at least download one image
+    #The program doesn't suck that bad
+    #But yeah, this seemed easier to implement
     i = 1
-    soup = BeautifulSoup(plain_text, "html.parser")
-    loc = input("Enter storage location : ")
 
+    #Find all links whose class is fileThumb
     for link in soup.findAll('a', {'class': 'fileThumb'}):
+        #Download only the images that are greater than/equal to the resume value
         if i >= res_val :
+            #Get the address of the links in string form
             address = str(link.get('href'))
             name = address[15:]
+
+            #log file writing
             fwr = open(loc + "\\" + logname + ".txt", 'w')
             if downloader(address, loc):
                 print(i)
@@ -43,3 +53,4 @@ resume_val = int(input("""Did the program halt?
 If yes, then at what point?
 If no, just hit enter.""") or "1")
 crawler(url, resume_val)
+print("Done with the downloads!")
